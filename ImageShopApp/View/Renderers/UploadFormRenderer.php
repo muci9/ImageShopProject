b7c96db18f4424508236eebcc119b48d26615cd3
muresanciprian99 <muresan.ciprian99@gmail.com>
2019-07-31 16:54:16 +0300

    add UploadFormRenderer

--- ImageShopApp/View/Renderers/UploadFormRenderer.php
+++ ImageShopApp/View/Renderers/UploadFormRenderer.php
@@ -1,10 +1,20 @@
 <?php
 
-
 namespace ImageShopApp\View\Renderers;
 
 
-class UploadFormRenderer
+class UploadFormRenderer implements IRenderer
 {
+    private $tags;
 
+    public function __construct(array $tags)
+    {
+        $this->tags = $tags;
+    }
+
+    public function render() : void
+    {
+        $tagCollection = $this->tags;
+        require_once "ImageShopApp/View/Templates/upload-form.php";
+    }
 }
