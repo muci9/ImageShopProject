d0eeae3278fc9f3e90c5c3f661bcf0b383709e33
Ciprian-Adrian Muresan <muresan.ciprian99@gmail.com>
2019-07-31 21:25:14 +0300

    add user controller

--- ImageShopApp/Controller/UserController.php
+++ ImageShopApp/Controller/UserController.php
@@ -4,7 +4,23 @@
 namespace ImageShopApp\Controller;
 
 
+use ImageShopApp\Model\Persistence\Finder\UserFinder;
+use ImageShopApp\Model\Persistence\PersistenceFactory;
+
 class UserController
 {
-
+    public static function showUser()
+    {
+        $userFinder = PersistenceFactory::createFinder('User');
+        /**
+         * @var UserFinder $userFinder
+         */
+        $user = $userFinder->findUserByEmail('b');
+        if (!$user) {
+            echo "User not found";
+            die;
+        }
+        echo $user->getName();
+        die;
+    }
 }
