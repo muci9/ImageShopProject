988732f009c7596efd7560668f2bcbf47a539425
muresanciprian99 <muresan.ciprian99@gmail.com>
2019-07-31 16:53:14 +0300

    add entity to represent a tag from the database

--- ImageShopApp/Model/DomainObject/Tag.php
+++ ImageShopApp/Model/DomainObject/Tag.php
@@ -6,5 +6,22 @@ namespace ImageShopApp\Model\DomainObject;
 
 class Tag
 {
+    private $id;
+    private $name;
 
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    public function getTagName() : string
+    {
+        return $this->name;
+    }
 }
