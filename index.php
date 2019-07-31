3daeab7b1ad842221f9e93a7f3881367f3751b4f
muresanciprian99 <muresan.ciprian99@gmail.com>
2019-07-31 14:39:57 +0300

    add primitive index.php file for routes testing

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "vendor/autoload.php";
+$config = require 'config.php';
+$request = $_SERVER['REQUEST_URI'];
+use ImageShopApp\Controller\ProductController;
+
+switch ($request) {
+    case '/':
+        //ProductController::showProducts();
+        break;
+    case '/product/show':
+        break;
+}
