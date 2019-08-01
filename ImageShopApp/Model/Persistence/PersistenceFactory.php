c71b66fc1493bc456722cda09011a69348efe3f6
Ciprian-Adrian Muresan <muresan.ciprian99@gmail.com>
2019-08-02 02:04:07 +0300

    small change to PersistenceFactory

--- ImageShopApp/Model/Persistence/PersistenceFactory.php
+++ ImageShopApp/Model/Persistence/PersistenceFactory.php
@@ -6,6 +6,12 @@ use ImageShopApp\Model\Persistence\Finder\AbstractFinder;
 use ImageShopApp\Model\Persistence\Finder\ProductFinder;
 use ImageShopApp\Model\Persistence\Finder\TagFinder;
 use ImageShopApp\Model\Persistence\Finder\UserFinder;
+use ImageShopApp\Model\Persistence\Mapper\AbstractMapper;
+use ImageShopApp\Model\Persistence\Mapper\OrderItemMapper;
+use ImageShopApp\Model\Persistence\Mapper\ProductMapper;
+use ImageShopApp\Model\Persistence\Mapper\ProductTagMapper;
+use ImageShopApp\Model\Persistence\Mapper\UserMapper;
+use ImageShopApp\Model\Persistence\Finder\ProductTagFinder;
 use PDO;
 
 class PersistenceFactory
@@ -21,6 +27,30 @@ class PersistenceFactory
             self::$pdo = new PDO($config['dsn'], $config['user'], $config['password']);
         }
         return self::$pdo;
+    }
+
+    public static function createMapper(string $entityClass): AbstractMapper
+    {
+        $mapperClass = self::getMapperClassName($entityClass);
+        // we ensure we create a single mapper instance of this type per request
+        if (!isset(self::$mappers[$mapperClass])) {
+            self::$mappers[$mapperClass] = new $mapperClass(self::createPdo());
+        }
+        return self::$mappers[$mapperClass];
+    }
+
+    private static function getMapperClassName(string $entityClass) : string
+    {
+        $entityClass = mb_strtolower($entityClass);
+        if ($entityClass === "product")
+            return ProductMapper::class;
+        if ($entityClass === "user")
+            return UserMapper::class;
+        if ($entityClass === "producttag")
+            return ProductTagMapper::class;
+        if ($entityClass === "orderitem")
+            return OrderItemMapper::class;
+        return null;
     }
 
     public static function createFinder(string $entityClass) : AbstractFinder
@@ -41,6 +71,10 @@ class PersistenceFactory
             return UserFinder::class;
         if ($entityClass === "tag")
             return TagFinder::class;
+        if ($entityClass === "producttag")
+            return ProductTagFinder::class;
+        //if ($entityClass === "orderitem")
+        //  return OrderItemFinder::class;
         return null;
     }
 }
