2c9ddbe5dc533adf22bd21e3ce2482b5e9020965
Ciprian-Adrian Muresan <muresan.ciprian99@gmail.com>
2019-08-02 00:08:50 +0300

    change config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'user' => 'ciprianmuresan',
-    'password' => 'root',
+    'user' => 'cipmures',
+    'password' => '1905',
     'dsn' => 'mysql:host=localhost;port=3306;dbname=db_artists_shop'
 ];
